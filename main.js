879af78766e5bc2634395b9a9b47f8eb5a1bc000
Kevin Liu <134642275+kcliu8035@users.noreply.github.com>
2024-01-08 13:31:49 -0500

    Added color picking functionality and changed aesthetics

--- main.js
+++ main.js
@@ -4,22 +4,27 @@ const reset = document.getElementById('reset');
 const black = document.getElementById('black');
 const red = document.getElementById('red');
 const eraser = document.getElementById('eraser');
+const custom = document.getElementById('custom');
+const random = document.getElementById('randomBTN');
+let color = 'black';
+let click = false;
 
 function makeDivs(size) {
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 
-    // To remove any existing divs if board is going to change
-    let existingDivs = document.querySelectorAll('div');
-    existingDivs.forEach((div) => div.remove())
+// To remove any existing divs if board is going to change
+    // let existingDivs = document.querySelectorAll('div');
+    // existingDivs.forEach((div) => div.remove())
 
     for (let i = 0; i < (size * size); i++) {
         const div = document.createElement('div'); 
         board.insertAdjacentElement("beforeend", div);
 
-
+    // Create separate function to input here than changes color by setting input to user choice
         div.addEventListener("mouseover", (e) => {
-            div.style.backgroundColor = 'black';
+            div.style.backgroundColor = color
+            document.body.style.cursor = 'eraser.png'
         })
     }
 }
@@ -27,16 +32,27 @@ makeDivs(36);
 
 
 reset.addEventListener('click', () => {
-    board.innerHTML = '';
+    let divs = board.querySelectorAll('div');
+    divs.forEach((div) => div.style.backgroundColor = '#c0c0c0');
 })
 
 
-eraser.addEventListener('click', () => {
-    div.style.backgroundColor = 'white';
+//Changes color
+document.addEventListener('coloris:pick', event => {
+    color = event.detail.color;
+});
+
+custom.addEventListener('click', () => {
+    let input = prompt('Pick a value');
 })
 
-
+random.addEventListener('click', () => {
+    color = `hsl(${Math.random() * 360}, 100%, 50%)`
+})
 
 // NOTES:
-// - Find out how to switch color of divs when "div" is not global variable 
+// - (DONE) Find out how to switch color of divs when "div" is not global variable 
+// - Allow user to change size of board
+// - Change cursor to eraser when it is chosen
+// - Create pallette or selection for colors
 
